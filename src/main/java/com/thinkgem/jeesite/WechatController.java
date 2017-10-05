d0c6eb7d64b1c48b5203d489ff4ab046e3021830
11653786@qq.com <11653786@qq.com>
2017-10-05 13:10:49 +0800

    成千上万的bug

--- src/main/java/com/thinkgem/jeesite/WechatController.java
+++ src/main/java/com/thinkgem/jeesite/WechatController.java
@@ -1,7 +1,13 @@
 package com.thinkgem.jeesite;
 
+import com.thinkgem.jeesite.modules.manager.cabinet.entity.Cabinet;
+import com.thinkgem.jeesite.modules.manager.cabinet.service.CabinetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/10/5.
@@ -10,30 +16,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/api/wechat")
 public class WechatController {
 
+    @Autowired
+    private CabinetService cabinetService;
+
     /**
-     *  美食介绍
+     * 美食介绍
+     *
      * @return
      */
     @RequestMapping(value = "/food")
-    public String food(){
+    public String food() {
         return "wechat/food";
     }
 
     /**
      * 微信公众号品牌页面
+     *
      * @return
      */
     @RequestMapping(value = "/brand")
-       public String brand(){
+    public String brand() {
         return "wechat/pinpai";
     }
 
     /**
      * 附近快餐柜
+     *
      * @return
      */
     @RequestMapping(value = "/allcabinet")
-    public String allcabinet(){
+    public String allcabinet(Model model) {
+        List<Cabinet> list = cabinetService.findList(new Cabinet());
+        model.addAttribute("cabinets", list);
         return "wechat/allcabinet";
     }
 }
