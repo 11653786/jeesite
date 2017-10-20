781de935bbb2ade31360276d4913f434c1d56070
杨涛 <yangtao@etiaolong.com>
2017-10-20 17:14:30 +0800

    mina

--- src/main/java/com/thinkgem/jeesite/modules/act/rest/servlet/FilterServletOutputStream.java
+++ src/main/java/com/thinkgem/jeesite/modules/act/rest/servlet/FilterServletOutputStream.java
@@ -28,5 +28,13 @@ public class FilterServletOutputStream extends ServletOutputStream {
 	}
 
 
+	@Override
+	public boolean isReady() {
+		return false;
+	}
 
+	@Override
+	public void setWriteListener(WriteListener writeListener) {
+
+	}
 }
