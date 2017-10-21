bf56b35a5103da566b599f6a072bd2db8b4fe649
11653786@qq.com <11653786@qq.com>
2017-10-21 14:50:40 +0800

    成千上万的bug

--- src/main/java/com/thinkgem/jeesite/mina/SocketServer.java
+++ src/main/java/com/thinkgem/jeesite/mina/SocketServer.java
@@ -9,14 +9,14 @@ public class SocketServer {
         try {
             ServerSocket ss = new ServerSocket(8888);
             System.out.println("启动服务器....");
-            Socket s = ss.accept();
-            System.out.println("客户端:" + s.getInetAddress().getLocalHost() + ":" + s.getPort() + "已连接到服务器");
+            Socket client = ss.accept();
+            System.out.println("客户端:" + client.getInetAddress().getLocalHost() + ":" + client.getPort() + "已连接到服务器");
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
             //读取客户端发送来的消息
             String mess = br.readLine();
             System.out.println("客户端：" + mess);
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
             bw.write(mess + "\n");
             bw.flush();
         } catch (IOException e) {
