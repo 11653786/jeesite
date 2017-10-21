d4f7a63e1f4dfa3155f90962dd18476c711262e6
11653786@qq.com <11653786@qq.com>
2017-10-21 14:50:52 +0800

    成千上万的bug

---
+++ src/main/java/com/thinkgem/jeesite/mina/SocketListener.java
@@ -0,0 +1,24 @@
+package com.thinkgem.jeesite.mina;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Created by Administrator on 2017/10/21.
+ */
+public class SocketListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        //初始化socket
+//        Server socketServer=new Server();
+//        socketServer.init();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+    }
+
+
+}
