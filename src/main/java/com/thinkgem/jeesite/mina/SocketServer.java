8795752b2a3ae7d0518a14b14d2eba36fd972c7e
11653786@qq.com <11653786@qq.com>
2017-10-21 15:18:41 +0800

    成千上万的bug

--- src/main/java/com/thinkgem/jeesite/mina/SocketServer.java
+++ src/main/java/com/thinkgem/jeesite/mina/SocketServer.java
@@ -1,24 +1,39 @@
 package com.thinkgem.jeesite.mina;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SocketServer {
+
+    private static int port=8888;
+
+    private static final Log log = LogFactory.getLog(SocketServer.class);
+
     public static void main(String[] args) {
         try {
-            ServerSocket ss = new ServerSocket(8888);
-            System.out.println("启动服务器....");
-            Socket client = ss.accept();
-            System.out.println("客户端:" + client.getInetAddress().getLocalHost() + ":" + client.getPort() + "已连接到服务器");
+            ServerSocket server = new ServerSocket(8888);
+            log.info("启动Socket: "+server.getInetAddress().getHostAddress()+":"+server.getLocalPort());
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            //读取客户端发送来的消息
-            String mess = br.readLine();
-            System.out.println("客户端：" + mess);
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-            bw.write(mess + "\n");
-            bw.flush();
+
+            while (true) {
+                Socket client = server.accept();
+                if(client!=null){
+                    log.info("客户端:" + client.getInetAddress().getLocalHost() + ":" + client.getPort() + "已连接到服务器");
+                    //读取客户端发送来的消息
+                    BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                    String mess = br.readLine();
+                    log.info("客户端发来的消息：" + mess);
+                    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+                    bw.write(mess + "\n");
+                    log.info("服务返回消息：" + mess);
+                    bw.flush();
+                }
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
